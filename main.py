ed678c571c60e868ed2589d15695efd222d5533e
Darwin Arevalo <dalexis.da@gmail.com>
2021-04-28 14:34:17 -0600

    App run

--- main.py
+++ main.py
@@ -87,3 +87,7 @@ def report_user():
 @app.route('/report', methods=["GET"])
 def send_report():
     return send_file("report.pdf", attachment_filename="user_report.pdf")
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
